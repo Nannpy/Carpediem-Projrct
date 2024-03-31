e63fa433648d39edd2835dc076512a420fcb43b3
Nannpy <131631162+Nannpy@users.noreply.github.com>
2024-03-31 15:05:18 +0700

    frist draft

---
+++ App.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# Table
+df = pd.DataFrame({
+    'first column' : [1,2,3,4],
+    'second column' : [10,20,30,40]
+})
+
+st.write('Here is our first table')
+st.write(df)
+
+# chart
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+# map
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
